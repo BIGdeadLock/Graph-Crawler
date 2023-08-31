9faf361b090c7ef577d4d593581d1738678f1709
BIGdeadLock <64005996+BIGdeadLock@users.noreply.github.com>
2023-08-31 13:57:27 +0300

    Update log.py

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,9 +1,4 @@
-# IBM Confidential - OCO Source Materials
-# (C) Copyright IBM Corp. 2020
-# The source code for this program is not published or otherwise divested of its trade secrets,
-# irrespective of what has been deposited with the U.S. Copyright Office.
 
-__author__ = "IBM"
 
 import logging as log
 from datetime import datetime
